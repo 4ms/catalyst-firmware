dce79c893435800b185f29e3319f81bba609bd8d
audiobird <cantrellhepner@gmail.com>
2023-08-03 13:34:58 -0700

    add outputs routine

--- src/outputs.hh
+++ src/outputs.hh
@@ -3,6 +3,8 @@
 #include "conf/model.hh"
 #include "drivers/spi.hh"
 
+using namespace mdrivlib;
+
 namespace Catalyst2
 {
 
@@ -13,12 +15,40 @@ public:
 	{
 		// sets up peripheral using pins/config from board_conf.hh
 		// use Board::DacConf
+		spi.configure();
+		spi.enable();
 	}
 
 	void write(const Model::OutputBuffer &out)
 	{
 		// write to the DAC
+		uint8_t data_buffer[3];
+
+		for (auto chan = 0u; chan < Model::NumChans; chan++)
+		{
+			data_buffer[2] = write_command | chan;
+			data_buffer[1] = static_cast<uint8_t>(out[chan] >> 8);
+			data_buffer[0] = static_cast<uint8_t>(out[chan] & 0xff);
+
+			spi.select<0>();
+
+			for (auto data_byte = 0; data_byte < 3; data_byte++)
+			{
+				while(!spi.tx_space_available())
+				;
+				spi.load_tx_data(data_buffer[data_byte]);
+				// spi.start_transfer(); //not necessary ?
+			}	
+			spi.unselect<0>();
+		}
 	}
+
+private:
+	//write to and update dac channel N
+	//datasheet page 20
+	static constexpr uint8_t write_command = 0b00110000; 
+	
+	SpiPeriph<Board::DacSpiConf> spi;
 };
 
 } // namespace Catalyst2
