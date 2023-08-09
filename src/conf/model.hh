b8c520bb2dbc57b2a825f6ebdda578f3ba8f84d9
audiobird <cantrellhepner@gmail.com>
2023-08-09 11:47:40 -0700

    should these additions be in board conf?

--- src/conf/model.hh
+++ src/conf/model.hh
@@ -1,4 +1,5 @@
 #pragma once
+#include <algorithm>
 #include <array>
 #include <cstdint>
 
@@ -15,5 +16,16 @@ using OutputBuffer = std::array<uint16_t, NumChans>;
 enum class ModeSwitch { Sequence, Macro };
 
 enum class AdcElement { Slider, CVJack };
+
+static constexpr float max_output_voltage = 10.f;
+static constexpr float min_output_voltage = -5.f;
+
+constexpr uint16_t volts_to_uint(const float volts)
+{
+	auto temp = std::clamp(volts, min_output_voltage, max_output_voltage);
+	temp -= min_output_voltage;
+	temp /= max_output_voltage - min_output_voltage;
+	return static_cast<uint16_t>(temp * 65535u);
+}
 
 } // namespace Catalyst2::Model
