87e548d22d0cf901b4db5f394c6cab5d2033132b
audiobird <cantrellhepner@gmail.com>
2023-08-27 11:20:59 -0700

    change morph function to a mathtools func, maybe we add it to util/math.hh

--- src/macro_seq.hh
+++ src/macro_seq.hh
@@ -6,6 +6,27 @@
 #include "util/math.hh"
 #include <algorithm>
 
+namespace MathTools
+{
+// if slope == 0 actual slope == 1
+// if slope is >= (max - min) actual slope == inf
+// if slope is negative than actual slope is less than 1
+constexpr float slope_adj(float in, float slope, float min, float max)
+{
+	auto range = max - min;
+	auto b = range / 2.f;
+
+	if (slope >= range) {
+		return in < b ? min : max;
+	}
+
+	auto m = range / (range - slope);
+	auto x = in - b;
+	auto y = m * x + b;
+	return std::clamp(y, min, max);
+}
+} // namespace MathTools
+
 namespace Catalyst2
 {
 
@@ -30,7 +51,7 @@ public:
 
 			auto phase = params.pos;
 			phase = params.pathway.adjust_and_scale(phase);
-			phase = do_morph(phase);
+			phase = MathTools::slope_adj(phase, params.morph_step, 0.f, 1.f);
 
 			for (auto [chan, out] : countzip(buf)) {
 				auto a = params.banks.get_chan(left, chan);
@@ -51,18 +72,25 @@ public:
 	}
 
 private:
-	float do_morph(float in)
-	{
-		auto morph = params.morph_step;
-
-		// this avoids dividing by 0
-		if (morph >= 1.f) {
-			return in < .5f ? 0.f : 1.f;
-		}
-
-		morph = ((1.f / (1.f - morph)) * (in - .5f)) + .5f;
-		return std::clamp(morph, 0.f, 1.f);
-	}
+	// this is really just a slope adjustment function
+	// m = 1/1 -> ~1/.0000001
+	// TODO: maybe this can become a math utils function
+	// it would need more parameters
+	// slope == 0, m = 1
+	// slope == .999, m = 1000
+	// not using for now, see function at top of this file
+	// float do_morph(float in, float slope)
+	// {
+	// 	// this avoids dividing by 0
+	// 	if (slope >= 1.f) {
+	// 		return in < .5f ? 0.f : 1.f;
+	// 	}
+	// 	auto m = (1.f / (1.f - slope));
+	// 	auto x = in - .5f;
+	// 	static constexpr auto b = .5f;
+	// 	auto y = m * x + b;
+	// 	return std::clamp(y, 0.f, 1.f);
+	// }
 };
 
 } // namespace Catalyst2
