9afcc020196db0f58f8909522c1620a2e221c6b4
audiobird <cantrellhepner@gmail.com>
2023-09-08 11:41:51 -0700

    bump

--- src/params.hh
+++ src/params.hh
@@ -4,6 +4,7 @@
 #include "quantizer.hh"
 #include "scene.hh"
 #include "sequencer.hh"
+#include <optional>
 
 namespace Catalyst2
 {
@@ -16,6 +17,9 @@ struct Params {
 	Pathway pathway;
 	Sequencer seq;
 	Quantizer<static_cast<std::size_t>(Model::output_octave_range)> quantizer;
+	int8_t current_scale = 0;
+
+	std::optional<uint8_t> override_output;
 
 	// coefficient of fading from scene to scene
 	// f(x) = (1.f / (1.f - morph_step)) * x;
