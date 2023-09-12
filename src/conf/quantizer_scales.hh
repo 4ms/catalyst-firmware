d918435193954e06786d59cf712ac24d1e1f17ef
audiobird <cantrellhepner@gmail.com>
2023-09-12 09:02:19 -0700

    add some scales

--- src/conf/quantizer_scales.hh
+++ src/conf/quantizer_scales.hh
@@ -15,13 +15,17 @@ static constexpr ScalePalette off = {Palette::off, Scale{}};
 static constexpr ScalePalette chromatic = {Palette::grey, Scales::chromatic};
 static constexpr ScalePalette major = {Palette::green, Scales::major};
 static constexpr ScalePalette minor = {Palette::red, Scales::minor};
-static constexpr ScalePalette wholetone = {Palette::magenta, Scale{0.f, 2.f, 4.f, 8.f, 10.f}};
+static constexpr ScalePalette major_pentatonic = {Palette::orange, Scale{0.f, 2.f, 4.f, 7.f, 9.f}};
+static constexpr ScalePalette minor_pentatonic = {Palette::purple, Scale{0.f, 3.f, 5.f, 7.f, 10.f}};
+static constexpr ScalePalette wholetone = {Palette::cyan, Scale{0.f, 2.f, 4.f, 8.f, 10.f}};
 
 static constexpr std::array Scales = {
 	off,
 	chromatic,
 	major,
 	minor,
+	major_pentatonic,
+	minor_pentatonic,
 	wholetone,
 };
 
