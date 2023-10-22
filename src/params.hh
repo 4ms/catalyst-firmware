392152a24873d678c5c5cbee3ffeb41f902a27ec
audiobird <cantrellhepner@gmail.com>
2023-10-22 12:17:35 -0700

    move objects that were residing in ui into params. add interface to that separates macro and sequencer mode. separate data and interface for saving purposes

--- src/params.hh
+++ src/params.hh
@@ -1,31 +1,212 @@
 #pragma once
+#include "intclock.hh"
 #include "pathway.hh"
 #include "quantizer.hh"
+#include "recorder.hh"
 #include "scene.hh"
 #include "sequencer.hh"
+#include <array>
 #include <optional>
 
 namespace Catalyst2
 {
 
-// Params holds all the modes, settings and parameters
-// Params are set by UI, based on reading of the user input
-struct Params {
-	Banks banks;
-	Sequencer seq;
-	using Quan = Quantizer<static_cast<std::size_t>(Model::output_octave_range)>;
-	std::array<Quan, Model::NumChans> quantizer_bank;
+class SharedInterface {
+	using QuantizerArray = std::array<Quantizer<static_cast<uint32_t>(Model::output_octave_range)>, Model::NumChans>;
+	class DisplayHanger {
+		static constexpr uint32_t time_ms = 2000;
+		uint8_t onto;
+		uint32_t start_time;
 
+	public:
+		void Cancel()
+		{
+			onto = 0xff;
+		}
+		void Set(uint8_t encoder, uint32_t time_now)
+		{
+			start_time = time_now;
+			onto = encoder;
+		}
+		std::optional<uint8_t> Check(uint32_t time_now)
+		{
+			if (time_now - start_time >= 2000)
+				onto = 0xff;
+
+			if (onto == 0xff)
+				return std::nullopt;
+
+			return onto;
+		}
+	};
+	float pos;
+	ClockDivider::type clockdiv = 0;
+
+public:
+	QuantizerArray quantizer;
+	ClockDivider clockdivider;
+	RandomPool randompool;
 	std::optional<uint8_t> override_output;
+	DisplayHanger hang;
 
-	// coefficient of fading from scene to scene
-	// f(x) = (1.f / (1.f - morph_step)) * x;
-	float morph_step = 1.f;
-	float pos = 0.f;
+	void SetPos(float pos)
+	{
+		this->pos = pos;
+	}
 
-	// TODO: clarify these and name them better, and add/remove as needed:
+	float GetPos()
+	{
+		return pos;
+	}
+
+	void IncClockDiv(int32_t inc)
+	{
+		clockdiv = ClockDivider::IncDivIdx(clockdiv, inc);
+	}
+
+	ClockDivider::type GetClockDiv()
+	{
+		return ClockDivider::GetDivFromIdx(clockdiv);
+	}
+};
+
+namespace MacroMode
+{
+struct Data {
+	std::array<Pathway::Data, Model::NumBanks> pathway;
+	std::array<Bank::Data, Model::NumBanks> bank;
+};
+
+class Interface {
+	Data &data;
+	float morph = 1.f;
+	uint8_t cur_bank = 0;
+
+public:
+	SharedInterface &shared;
+	Pathway::Interface pathway;
+	Bank::Interface bank;
+	Recorder recorder;
+
+	Interface(Data &data, SharedInterface &shared)
+		: data{data}
+		, shared{shared}
+		, bank{shared.randompool}
+	{
+		SelectBank(0);
+	}
+	void SelectBank(uint8_t bank)
+	{
+		if (bank >= Model::NumBanks)
+			return;
+
+		this->bank.Load(data.bank[bank]);
+		this->pathway.Load(data.pathway[bank]);
+
+		for (auto [i, q] : countzip(shared.quantizer))
+			q.load_scale(this->bank.GetChannelMode(i).GetScale());
+
+		cur_bank = bank;
+	}
+	uint8_t GetSelectedBank()
+	{
+		return cur_bank;
+	}
+	void IncMorph(int32_t inc)
+	{
+		morph += (1.f / 100.f) * inc;
+		morph = std::clamp(morph, 0.f, 1.f);
+	}
+	float GetMorph()
+	{
+		return morph;
+	}
+};
+} // namespace MacroMode
+
+namespace SeqMode
+{
+using Data = Sequencer::Data;
+
+class Interface {
+	uint8_t cur_sequence = 0;
+	uint8_t cur_page = Model::SeqPages;
+
+public:
+	SharedInterface &shared;
+	Sequencer::Interface seq;
+	InternalClock<1000> internalclock;
+
+	Interface(Data &data, SharedInterface &shared)
+		: shared{shared}
+		, seq{data, shared.randompool}
+	{}
+
+	void SelectSequence(uint8_t sequence)
+	{
+		if (sequence >= Model::NumChans)
+			return;
+		else
+			cur_sequence = sequence;
+	}
+	uint8_t GetSelectedSequence()
+	{
+		return cur_sequence;
+	}
+	void DeselectSequence()
+	{
+		cur_sequence = Model::NumChans;
+	}
+	bool IsSequenceSelected()
+	{
+		return cur_sequence != Model::NumChans;
+	}
+	void SelectPage(uint8_t page)
+	{
+		if (page >= Model::SeqPages)
+			return;
+		else
+			cur_page = page;
+	}
+	uint8_t GetSelectedPage()
+	{
+		return cur_page;
+	}
+	void DeselectPage()
+	{
+		cur_page = Model::SeqPages;
+	}
+	bool IsPageSelected()
+	{
+		return cur_page != Model::SeqPages;
+	}
+	void IncStep(uint8_t step, int32_t inc, bool fine)
+	{
+		const auto page = IsPageSelected() ? GetSelectedPage() : seq.GetPlayheadPage(cur_sequence);
+		step += (page * Model::SeqStepsPerPage);
+		seq.Channel(cur_sequence).IncStep(step, inc, fine);
+	}
+	void IncStepMorph(uint8_t step, int32_t inc)
+	{
+		const auto page = IsPageSelected() ? GetSelectedPage() : seq.GetPlayheadPage(cur_sequence);
+		step += (page * Model::SeqStepsPerPage);
+		seq.Channel(cur_sequence).IncMorph(step, inc);
+	}
+};
+} // namespace SeqMode
+
+struct Params {
 	enum class Mode : bool { Sequencer, Macro };
 	Mode mode = Mode::Macro;
+
+	struct Data {
+		SeqMode::Data seq;
+		MacroMode::Data macro;
+	} data;
+	SharedInterface shared;
+
+	SeqMode::Interface sequencer{data.seq, shared};
+	MacroMode::Interface macro{data.macro, shared};
 };
 
 } // namespace Catalyst2
