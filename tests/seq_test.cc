c6d37002eddf32f63a8002bbeda69040e8f32e0a
audiobird <cantrellhepner@gmail.com>
2023-11-20 11:52:44 -0800

    notes and variable

--- tests/seq_test.cc
+++ tests/seq_test.cc
@@ -6,7 +6,7 @@
 using namespace Catalyst2;
 
 /*
-floating point error test. sequencer crashes if mode is ping pong and length == 1
+floating point error test. sequencer crashes if (mode == ping pong && length == 1)
 
 // bug: e8296861a9850b32b98ac56c21036c35b36c2f3a
 // unit test passes: b3e5aacc1990699af8339e83406d95f9228d49a6
@@ -28,7 +28,7 @@ TEST_CASE("Pingpong length bug") {
 	while (data.global.length.Read().value() != Model::MinSeqSteps)
 		data.global.length.Inc(-1);
 
-	CHECK(data.global.length.Read().value() == 1);
+	CHECK(data.global.length.Read().value() == Model::MinSeqSteps);
 
 	for (auto i = 0u; i < 1000u; i++) {
 		seq.player.Step();
