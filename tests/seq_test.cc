df6646a884aed8d23c4d8a4fd162c2894ea81cff
audiobird <cantrellhepner@gmail.com>
2023-11-20 11:51:01 -0800

    notes

--- tests/seq_test.cc
+++ tests/seq_test.cc
@@ -8,7 +8,9 @@ using namespace Catalyst2;
 /*
 floating point error test. sequencer crashes if mode is ping pong and length == 1
 
-// bug at e8296861a9850b32b98ac56c21036c35b36c2f3a
+// bug: e8296861a9850b32b98ac56c21036c35b36c2f3a
+// unit test passes: b3e5aacc1990699af8339e83406d95f9228d49a6
+// need to test hw
 
 */
 
