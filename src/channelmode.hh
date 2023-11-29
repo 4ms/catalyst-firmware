3a3f91242bce5fc9ad2acbe48502be7f93d9b75d
audiobird <cantrellhepner@gmail.com>
2023-11-29 13:43:05 -0800

    change namespace, tidy up

--- src/channelmode.hh
+++ src/channelmode.hh
@@ -6,10 +6,10 @@
 #include <algorithm>
 #include <cstdint>
 
-namespace Catalyst2
+namespace Catalyst2::Channel
 {
 
-class ChannelMode {
+class Mode {
 	static constexpr std::array Scale = {
 		Quantizer::Scale{},																// none
 		Quantizer::Scale{0.f, 1.f, 2.f, 3.f, 4.f, 5.f, 6.f, 7.f, 8.f, 9.f, 10.f, 11.f}, // chromatic
@@ -20,12 +20,7 @@ class ChannelMode {
 		Quantizer::Scale{0.f, 2.f, 4.f, 8.f, 10.f},										// wholetone
 	};
 
-	// one is added for gate mode.
-	// unquantized is implicit (see the first scale in the array)
-	static constexpr uint8_t Count = Scale.size() + 1;
-
-	// the color that each channel mode is represented by
-	static constexpr std::array<Color, Count> color = {
+	static constexpr std::array color = {
 		Color{2, 2, 0},	  // off
 		Palette::magenta, // chromatic
 		Palette::blue,	  // major
@@ -36,29 +31,27 @@ class ChannelMode {
 		Palette::green,	  // gate
 	};
 
-	uint8_t val = 0;
+	static constexpr uint8_t max = color.size() - 1;
+	static constexpr uint8_t min = 0u;
+	uint8_t val = min;
 
 public:
 	void Inc(int32_t dir) {
 		auto temp = val + dir;
-		val = std::clamp<int32_t>(temp, 0, Count - 1);
+		val = std::clamp<int32_t>(temp, min, max);
 	}
-
 	bool IsGate() {
-		return val == Count - 1;
+		return val == max;
 	}
-
 	bool IsQuantized() {
 		return !IsGate();
 	}
-
 	Quantizer::Scale GetScale() {
 		return Scale[IsGate() ? 0 : val];
 	}
-
 	Color GetColor() {
 		return color[val];
 	}
 };
 
-} // namespace Catalyst2
+} // namespace Catalyst2::Channel
