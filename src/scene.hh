7186302ce220b4fa6622a9527a906af1b04e1995
audiobird <cantrellhepner@gmail.com>
2023-11-29 13:50:44 -0800

    Cleanup, add range

--- src/scene.hh
+++ src/scene.hh
@@ -1,6 +1,6 @@
 #pragma once
+#include "channel.hh"
 #include "channelmode.hh"
-#include "channelvalue.hh"
 #include "conf/model.hh"
 #include "pathway.hh"
 #include "randompool.hh"
@@ -14,11 +14,12 @@ namespace Catalyst2::Bank
 {
 struct Data {
 	struct Scene {
-		std::array<Channel, Model::NumChans> channel;
+		std::array<Channel::Value, Model::NumChans> channel;
 		float random_amount = 0; // 1.f / 15.f;
 	};
 	std::array<Scene, Model::NumScenes> scene;
-	std::array<ChannelMode, Model::NumChans> channelmode;
+	std::array<Channel::Mode, Model::NumChans> channelmode;
+	std::array<Channel::Range, Model::NumChans> range;
 };
 
 class Interface {
@@ -30,59 +31,51 @@ public:
 	Interface(RandomPool &r)
 		: randompool{r} {
 	}
-
 	void Load(Data &d) {
 		b = &d;
 	}
-
 	void Copy(uint8_t scene) {
 		clipboard = b->scene[scene];
 	}
-
 	void Paste(uint8_t scene) {
 		b->scene[scene] = clipboard;
 	}
-
 	void IncChannelMode(uint8_t channel, int32_t inc) {
 		b->channelmode[channel].Inc(inc);
 	}
-
-	ChannelMode GetChannelMode(uint8_t channel) {
+	void IncRange(uint8_t channel, int32_t inc) {
+		b->range[channel].Inc(inc);
+	}
+	Channel::Mode GetChannelMode(uint8_t channel) {
 		return b->channelmode[channel];
 	}
-
-	void SetChanMode(uint8_t channel, ChannelMode mode) {
+	void SetChanMode(uint8_t channel, Channel::Mode mode) {
 		b->channelmode[channel] = mode;
 	}
-
 	float GetRandomAmount(uint8_t scene) {
 		return b->scene[scene].random_amount;
 	}
-
 	void SetRandomAmount(uint8_t scene, float amount) {
 		b->scene[scene].random_amount = std::clamp(amount, 0.f, 1.f);
 	}
-
 	void IncRandomAmount(uint8_t scene, int32_t inc) {
 		auto i = inc / 50.f;
 		SetRandomAmount(scene, b->scene[scene].random_amount + i);
 	}
-
 	void IncChan(uint8_t scene, uint8_t channel, int32_t inc, bool fine) {
-		b->scene[scene].channel[channel].Inc(inc, fine, GetChannelMode(channel).IsGate());
+		b->scene[scene].channel[channel].Inc(inc, fine, GetChannelMode(channel).IsGate(), b->range[channel]);
 	}
-
-	ChannelValue::type GetChannel(uint8_t scene, uint8_t channel) {
+	Model::Output::type GetChannel(uint8_t scene, uint8_t channel) {
 
 		auto rand = static_cast<int32_t>(randompool.GetSceneVal(scene, channel) * b->scene[scene].random_amount *
-										 ChannelValue::Range);
+										 Channel::range);
 
 		if (GetChannelMode(channel).IsGate()) {
 			// gates not affected by randomness?
 			rand = 0;
 		}
 		auto temp = b->scene[scene].channel[channel].val + rand;
-		return std::clamp<int32_t>(temp, ChannelValue::Min, ChannelValue::Max);
+		return std::clamp<int32_t>(temp, Channel::min, Channel::max);
 	}
 };
 
