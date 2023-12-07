49ceced6e66b8fa00d96f1275c63650cc59aa39e
audiobird <cantrellhepner@gmail.com>
2023-12-07 11:05:34 -0800

    fixes template specialization error... not sure if we need it the way it was written

--- tests/settings.hh
+++ tests/settings.hh
@@ -7,28 +7,28 @@ template<typename SeqModeData, typename MacroModeData>
 class SavedSettings {
 
 public:
-	template<typename DataT>
-	bool read(DataT &data);
+	// template<typename DataT>
+	// bool read(DataT &data);
 
-	template<typename DataT>
-	bool write(DataT const &data);
+	// template<typename DataT>
+	// bool write(DataT const &data);
 
-	template<>
+	// template<>
 	bool read(SeqModeData &data) {
 		return true;
 	}
 
-	template<>
+	// template<>
 	bool write(SeqModeData const &data) {
 		return true;
 	}
 
-	template<>
+	// template<>
 	bool read(MacroModeData &data) {
 		return true;
 	}
 
-	template<>
+	// template<>
 	bool write(MacroModeData const &data) {
 		return true;
 	}
