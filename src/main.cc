b2a3304ebf7db1370dd96850480f977f649685f8
audiobird <cantrellhepner@gmail.com>
2023-12-28 13:53:43 -0800

    notes

--- src/main.cc
+++ src/main.cc
@@ -5,39 +5,24 @@
 #include "hardware_tests/hardware_tests.hh"
 #include "system.hh"
 #include "ui.hh"
+
 namespace
 {
 Catalyst2::System _init;
 } // namespace
 
 // maybe hold play to turn on loop?
-// do oneshot
 
 // TODO: make color fades appear more linear
 
 /* october 9th
-
 maybe clipboards should reset eachother?
-
-gate bright green needs fix
-
 */
 
-/*
-12/5
-
-make sure you can adjust random values even if range technically shouldnt let you
-val - clamp(val + inc, rangemin - random, rangemax - random)
-
-*/
 /*
 12/19
 bug when switch modes it will get stuck on bank mode leds or morph mode leds sometimes
 make play led work
-save tap tempo time on power off1
-hide playhead when adjusting that step
-make channel value 0 - 16384 instead of uint16 range so that it can go above and below the max without overflowing and
-without using more data
 */
 
 void main() {
@@ -47,7 +32,6 @@ void main() {
 	Ui::Interface ui{params};
 	MacroSeq macroseq{params};
 
-	// 7% at 1kHz, 21% at 3kHz
 	mdrivlib::Timekeeper cv_stream(Board::cv_stream_conf, [&macroseq, &ui]() {
 		ui.Update();
 		auto out = macroseq.Update();
