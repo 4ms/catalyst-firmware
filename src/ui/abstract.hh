d63c7dcf0933d60e9109a8de92f066242b4d3696
audiobird <cantrellhepner@gmail.com>
2023-12-28 14:11:58 -0800

    braces

--- src/ui/abstract.hh
+++ src/ui/abstract.hh
@@ -25,8 +25,9 @@ public:
 
 protected:
 	void ClearButtonLeds() {
-		for (auto i = 0u; i < Model::NumChans; i++)
+		for (auto i = 0u; i < Model::NumChans; i++) {
 			c.SetButtonLed(i, false);
+		}
 	}
 
 	void ClearEncoderLeds() {
@@ -36,29 +37,32 @@ protected:
 	}
 
 	void SetButtonLedsCount(uint8_t count, bool on) {
-		for (auto i = 0u; i < count; i++)
+		for (auto i = 0u; i < count; i++) {
 			c.SetButtonLed(i, on);
+		}
 	}
 
 	void SetEncoderLedsCount(uint8_t count, uint8_t offset, Color col) {
-		for (auto i = 0u; i < count; i++)
+		for (auto i = 0u; i < count; i++) {
 			c.SetEncoderLed((i + offset) & 7, col);
-
-		for (auto i = 0u; i < Model::NumChans - count; i++)
+		}
+		for (auto i = 0u; i < Model::NumChans - count; i++) {
 			c.SetEncoderLed((count + i + offset) & 7, Colors::off);
+		}
 	}
 
 	void SetEncoderLedsAddition(uint8_t num, Color col) {
 		static constexpr auto max_val = [] {
 			auto out = 0u;
-			for (auto i = 1u; i <= Model::NumChans; i++)
+			for (auto i = 1u; i <= Model::NumChans; i++) {
 				out += i;
+			}
 			return out;
 		}();
 
-		if (num > max_val)
+		if (num > max_val) {
 			return;
-
+		}
 		auto t = Model::NumChans;
 
 		while (num >= t) {
@@ -74,18 +78,18 @@ protected:
 		uint8_t youngest = 0xffu;
 
 		for (auto [i, b] : countzip(c.button.scene)) {
-			if (!b.is_high())
+			if (!b.is_high()) {
 				continue;
-
-			if (b.time_high > age)
+			}
+			if (b.time_high > age) {
 				continue;
-
+			}
 			age = b.time_high;
 			youngest = i;
 		}
-		if (youngest == 0xff)
+		if (youngest == 0xff) {
 			return std::nullopt;
-
+		}
 		return youngest;
 	}
 
