b78566a110ecdfc840c973b142425943cc3b9bc7
audiobird <cantrellhepner@gmail.com>
2024-01-05 15:02:25 -0800

    clock resets as it should now. see TODO

--- src/ui/seq_common.hh
+++ src/ui/seq_common.hh
@@ -6,6 +6,8 @@
 namespace Catalyst2::Sequencer::Ui
 {
 class Usual : public Catalyst2::Ui::Abstract {
+	uint32_t time_trigged = 0;
+
 public:
 	Sequencer::Interface &p;
 	Usual(Sequencer::Interface &p, Controls &c)
@@ -13,20 +15,22 @@ public:
 		, p{p} {
 	}
 	void Common() override final {
-		if (c.jack.reset.is_high()) {
+		if (c.jack.reset.just_went_high()) {
 			p.shared.internalclock.Reset();
 			p.shared.clockdivider.Reset();
 			p.player.Reset();
-			return;
+			time_trigged = p.shared.internalclock.TimeNow();
 		}
-
+		// TODO: this seems to work but the time_trigged variable is unnecessarily copied to each of sequencers modes...
 		if (c.jack.trig.just_went_high()) {
-			if (p.shared.internalclock.IsInternal()) {
-				p.shared.internalclock.SetExternal(true);
-			}
-			p.shared.clockdivider.Update(p.shared.data.clockdiv);
-			if (p.shared.clockdivider.Step()) {
-				p.shared.internalclock.Input();
+			if (p.shared.internalclock.TimeNow() - time_trigged >= Clock::BpmToTicks(1200)) {
+				if (p.shared.internalclock.IsInternal()) {
+					p.shared.internalclock.SetExternal(true);
+				}
+				p.shared.clockdivider.Update(p.shared.data.clockdiv);
+				if (p.shared.clockdivider.Step()) {
+					p.shared.internalclock.Input();
+				}
 			}
 		}
 
