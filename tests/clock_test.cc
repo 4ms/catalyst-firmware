40faa28f547e873736800196b7e872d172769a11
audiobird <cantrellhepner@gmail.com>
2024-01-09 15:09:41 -0800

    bump

--- tests/clock_test.cc
+++ tests/clock_test.cc
@@ -6,6 +6,7 @@ using namespace Catalyst2;
 
 TEST_CASE("clock phase") {
 	Clock::Bpm::type bpm;
+	bpm.Set(Clock::BpmToTicks(64000));
 	Clock::Bpm clock{bpm};
 
 	Clock::Divider::type cdiv;
@@ -15,14 +16,15 @@ TEST_CASE("clock phase") {
 
 	uint32_t step = 1;
 
+	fprintf(stderr, "C Div Phase: %f\n", divider.GetPhase(cdiv));
+
 	for (;;) {
 		clock.Update();
 
 		if (clock.Output()) {
 
-			fprintf(stderr, "Clock Divider Phase: %f\n", divider.GetPhase(cdiv));
-
 			divider.Update(cdiv);
+			fprintf(stderr, "C Div Phase: %f\n", divider.GetPhase(cdiv));
 
 			if (divider.Step()) {
 				step += 1;
@@ -31,5 +33,7 @@ TEST_CASE("clock phase") {
 			if (step == 2)
 				break;
 		}
+		fprintf(
+			stderr, "Clock Phase: %f Actual Phase: %f\n", clock.GetPhase(), divider.GetPhase(cdiv, clock.GetPhase()));
 	}
 }
