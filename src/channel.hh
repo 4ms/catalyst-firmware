ff6d7d3878e6c566925a0b073d02b4a7d14def7c
audiobird <cantrellhepner@gmail.com>
2024-01-10 10:42:56 -0800

    move range to its own file

--- src/channel.hh
+++ src/channel.hh
@@ -2,21 +2,12 @@
 
 #include "conf/model.hh"
 #include "random.hh"
+#include "range.hh"
 #include <algorithm>
-#include <array>
 #include <cstdint>
 
 namespace Catalyst2::Channel
 {
-
-constexpr Model::Output::type max = UINT16_MAX;
-constexpr Model::Output::type min = 0;
-constexpr Model::Output::type range = max - min;
-
-constexpr Model::Output::type from_volts(const float volts) {
-	auto v = std::clamp(volts, Model::min_output_voltage, Model::max_output_voltage);
-	return MathTools::map_value(v, Model::min_output_voltage, Model::max_output_voltage, min, max);
-}
 
 constexpr auto gatehigh = from_volts(5.f);
 constexpr auto gatearmed = from_volts(0.f) + 1u;
@@ -24,60 +15,6 @@ constexpr auto gateoff = gatearmed - 1u;
 
 constexpr auto octave = range / Model::output_octave_range;
 constexpr auto note = octave / 12;
-
-class Range {
-	struct Option {
-		float min, max;
-	};
-	static constexpr std::array options = {
-		Option{Model::min_output_voltage, Model::max_output_voltage},
-		Option{-5.f, 5.f},
-		Option{0.f, 10.f},
-		Option{0.f, 5.f},
-		Option{0.f, 3.f},
-	};
-	static_assert(
-		[] {
-			for (auto i : options) {
-				if (i.min < Model::min_output_voltage || i.max > Model::max_output_voltage)
-					return false;
-			}
-			return true;
-		}(),
-		"Range option cannot be outside the voltage range of the hardware");
-
-	static constexpr uint8_t min = 0u;
-	static constexpr uint8_t max = options.size() - 1;
-	static constexpr auto absmaxv = [] {
-		const auto min = Model::min_output_voltage < 0 ? -Model::min_output_voltage : Model::min_output_voltage;
-		const auto max = Model::max_output_voltage < 0 ? -Model::max_output_voltage : Model::max_output_voltage;
-		return max >= min ? max : min;
-	}();
-	uint8_t val = 0;
-
-public:
-	void Inc(int32_t inc) {
-		val = std::clamp<int32_t>(val + inc, min, max);
-	}
-	float NegAmount() const {
-		return std::abs(options[val].min / absmaxv);
-	}
-	float PosAmount() const {
-		return options[val].max / absmaxv;
-	}
-	Model::Output::type Min() const {
-		return from_volts(options[val].min);
-	}
-	Model::Output::type Max() const {
-		return from_volts(options[val].max);
-	}
-	Model::Output::type Clamp(Model::Output::type in) {
-		return std::clamp(in, Min(), Max());
-	}
-	bool Validate() const {
-		return val <= max;
-	}
-};
 
 class Value {
 	static constexpr auto inc_step_fine = 1;
@@ -134,7 +71,10 @@ public:
 
 private:
 	int32_t CalculateRandom(Range range, Random::Pool::type offset) const {
-		offset *= offset < 0.f ? range.NegAmount() : range.PosAmount();
+		const auto pa = range.PosAmount();
+		auto na = range.NegAmount();
+		na = na == 0.f ? -pa : na;
+		offset *= offset < 0.f ? na : pa;
 		return offset * max;
 	}
 };
---
+++ src/range.hh
@@ -0,0 +1,76 @@
+#pragma once
+
+#include "conf/model.hh"
+#include <algorithm>
+#include <cstdint>
+
+namespace Catalyst2::Channel
+{
+
+constexpr Model::Output::type max = UINT16_MAX;
+constexpr Model::Output::type min = 0;
+constexpr Model::Output::type range = max - min;
+
+constexpr Model::Output::type from_volts(const float volts) {
+	auto v = std::clamp(volts, Model::min_output_voltage, Model::max_output_voltage);
+	return MathTools::map_value(v, Model::min_output_voltage, Model::max_output_voltage, min, max);
+}
+
+class Range {
+	struct Option {
+		float min, max;
+	};
+	static constexpr std::array options = {
+		Option{Model::min_output_voltage, Model::max_output_voltage},
+		Option{-5.f, 5.f},
+		Option{0.f, 10.f},
+		Option{0.f, 5.f},
+		Option{0.f, 3.f},
+	};
+	static_assert(
+		[] {
+			for (auto i : options) {
+				if (i.min < Model::min_output_voltage || i.max > Model::max_output_voltage)
+					return false;
+			}
+			return true;
+		}(),
+		"Range option cannot be outside the voltage range of the hardware");
+
+	static constexpr uint8_t min = 0u;
+	static constexpr uint8_t max = options.size() - 1;
+	static constexpr auto absmaxv = [] {
+		const auto min = Model::min_output_voltage < 0 ? -Model::min_output_voltage : Model::min_output_voltage;
+		const auto max = Model::max_output_voltage < 0 ? -Model::max_output_voltage : Model::max_output_voltage;
+		return max >= min ? max : min;
+	}();
+	uint8_t val = 0;
+
+public:
+	void Inc(int32_t inc) {
+		val = std::clamp<int32_t>(val + inc, min, max);
+	}
+	float NegAmount() const {
+		return std::abs(options[val].min / absmaxv);
+	}
+	float PosAmount() const {
+		return options[val].max / absmaxv;
+	}
+	Model::Output::type Min() const {
+		return from_volts(options[val].min);
+	}
+	Model::Output::type Max() const {
+		return from_volts(options[val].max);
+	}
+	Model::Output::type Clamp(Model::Output::type in) {
+		return std::clamp(in, Min(), Max());
+	}
+	float GetOctaveRange() const {
+		return options[val].max - options[val].min;
+	}
+	bool Validate() const {
+		return val <= max;
+	}
+};
+
+} // namespace Catalyst2::Channel
