940aa391f76c96dac8b8edf106ecaa08a46bff4b
Dan Green <danngreen1@gmail.com>
2024-01-16 11:28:39 -0800

    Better Slew LPF algorithm

--- src/slider_slew.hh
+++ src/slider_slew.hh
@@ -11,21 +11,19 @@ struct Data {
 
 class Interface {
 	Data &data;
-
 	float coef;
 	float current{0.f};
 
-	static constexpr float MaxSlewCoef = 1.f / 10000.f;
-
-	static constexpr float EncoderStepSize = 0.01f;
+	static constexpr float MaxSlew = 100000.f;
+	static constexpr float EncoderStepSize = 1.f / 200.f;
 
 	static float CalcCoef(float slew) {
-		// 0..1 => 1 (no slew) .. MaxSlewCoef
-		// coef = 1.f + (MaxSlewCoef - 1.f) * data.slew;
-		if (slew == 0)
+		if (slew <= EncoderStepSize)
 			return 1.f;
-		slew = 1.f - slew;
-		return 1.f / (slew * 100.f + MaxSlewCoef);
+		if (slew >= (1.f - EncoderStepSize))
+			return 1.f / MaxSlew;
+
+		return 1.f / (slew * slew * MaxSlew);
 	}
 
 public:
