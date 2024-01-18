2183489cefccfa973bd7e29764cbf2323318871d
audiobird <cantrellhepner@gmail.com>
2024-01-18 11:46:00 -0800

    move playmode led annimation function so global and per channel settings can call it

--- src/ui/seq_common.hh
+++ src/ui/seq_common.hh
@@ -51,5 +51,32 @@ protected:
 	void ConfirmPaste(uint8_t led) {
 		ConfirmCopy(led);
 	}
+	void PlayModeLedAnnimation(Catalyst2::Sequencer::Settings::PlayMode::Mode pm, uint32_t time_now) {
+		using namespace Palette::Setting;
+		static constexpr auto animation_duration = static_cast<float>(Clock::MsToTicks(1000));
+		auto phase = (time_now / animation_duration);
+		phase -= static_cast<uint32_t>(phase);
+		Color col;
+
+		using enum Catalyst2::Sequencer::Settings::PlayMode::Mode;
+
+		if (pm == Forward) {
+			col = playmode_fwd.blend(Palette::off, 1.f - phase);
+		} else if (pm == Backward) {
+			col = playmode_bck.blend(Palette::off, phase);
+		} else if (pm == PingPong) {
+			if (phase < .5f) {
+				phase *= 2.f;
+				col = playmode_bck.blend(Palette::off, phase);
+			} else {
+				phase -= .5f;
+				phase *= 2.f;
+				col = playmode_fwd.blend(Palette::off, 1.f - phase);
+			}
+		} else {
+			col = Palette::Random::color(time_now >> 8);
+		}
+		c.SetEncoderLed(Model::SeqEncoderAlts::PlayMode, col);
+	}
 };
 } // namespace Catalyst2::Sequencer::Ui
