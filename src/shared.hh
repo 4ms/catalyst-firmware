cea61b644e2fbeb323b9c8ce38f5f16794a1ed53
audiobird <cantrellhepner@gmail.com>
2024-02-01 11:35:46 -0800

    remove references to internal clock and pass in time value instead

--- src/shared.hh
+++ src/shared.hh
@@ -28,21 +28,17 @@ class Interface {
 		static constexpr uint32_t duration = Clock::MsToTicks(4000);
 		uint8_t onto;
 		uint32_t start_time;
-		Clock::Bpm &internalclock;
 
 	public:
-		DisplayHanger(Clock::Bpm &ic)
-			: internalclock{ic} {
-		}
 		void Cancel() {
 			onto = 0xff;
 		}
-		void Set(uint8_t encoder) {
-			start_time = internalclock.TimeNow();
+		void Set(uint8_t encoder, uint32_t time_now) {
+			start_time = time_now;
 			onto = encoder;
 		}
-		std::optional<uint8_t> Check() {
-			if (internalclock.TimeNow() - start_time >= duration) {
+		std::optional<uint8_t> Check(uint32_t time_now) {
+			if (time_now - start_time >= duration) {
 				onto = 0xff;
 			}
 			if (onto == 0xff) {
@@ -53,27 +49,23 @@ class Interface {
 	};
 
 	class Blinker {
-		Clock::Bpm &internalclock;
 		uint32_t remaining = 0;
 		uint32_t blink_duration = Clock::MsToTicks(3000);
 		uint8_t led = 0;
 		uint32_t set_time;
 
 	public:
-		Blinker(Clock::Bpm &ic)
-			: internalclock{ic} {
-		}
-		void Set(uint8_t led, uint32_t num_blinks, uint32_t duration_ms) {
+		void Set(uint8_t led, uint32_t num_blinks, uint32_t duration_ms, uint32_t time_now) {
 			remaining = num_blinks * 2;
 			this->led = led;
 			blink_duration = Clock::MsToTicks(duration_ms) / num_blinks;
-			set_time = internalclock.TimeNow();
+			set_time = time_now;
 		}
-		void Update() {
+		void Update(uint32_t time_now) {
 			if (!remaining) {
 				return;
 			}
-			const auto t = internalclock.TimeNow();
+			const auto t = time_now;
 			if (t - set_time >= blink_duration) {
 				set_time = t;
 				remaining -= 1;
