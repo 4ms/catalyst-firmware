efb31be0aff19aeafb5c617b5db752a39d7d99bb
audiobird <cantrellhepner@gmail.com>
2024-02-03 15:11:07 -0800

    move member functions into namespace

--- src/ui/abstract.hh
+++ src/ui/abstract.hh
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "clock.hh"
 #include "conf/model.hh"
 #include "conf/palette.hh"
 #include "controls.hh"
@@ -7,6 +8,150 @@
 
 namespace Catalyst2::Ui
 {
+inline void ConfirmCopy(Shared::Interface &p, uint8_t led) {
+	p.blinker.Set(led, 8, 250, p.internalclock.TimeNow());
+}
+inline void ConfirmPaste(Shared::Interface &p, uint8_t led) {
+	ConfirmCopy(p, led);
+}
+inline std::optional<uint8_t> YoungestSceneButton(Controls &c) {
+	auto age = 0xffffffffu;
+	uint8_t youngest = 0xffu;
+
+	for (auto [i, b] : countzip(c.button.scene)) {
+		if (!b.is_high()) {
+			continue;
+		}
+		if (b.time_high > age) {
+			continue;
+		}
+		age = b.time_high;
+		youngest = i;
+	}
+	if (youngest == 0xff) {
+		return std::nullopt;
+	}
+	return youngest;
+}
+inline void ForEachEncoderInc(Controls &c, auto func) {
+	for (auto [i, enc] : countzip(c.encoders)) {
+		const auto inc = enc.read();
+		if (inc) {
+			func(i, inc);
+		}
+	}
+}
+inline void ForEachSceneButtonReleased(Controls &c, auto func) {
+	for (auto [i, b] : countzip(c.button.scene)) {
+		if (b.just_went_low()) {
+			func(i);
+		}
+	}
+}
+inline void ForEachSceneButtonPressed(Controls &c, auto func) {
+	for (auto [i, b] : countzip(c.button.scene)) {
+		if (b.just_went_high()) {
+			func(i);
+		}
+	}
+}
+inline void ClearButtonLeds(Controls &c) {
+	for (auto i = 0u; i < Model::NumChans; i++) {
+		c.SetButtonLed(i, false);
+	}
+}
+
+inline void ClearEncoderLeds(Controls &c) {
+	for (auto i = 0u; i < Model::NumChans; i++) {
+		c.SetEncoderLed(i, Palette::off);
+	}
+}
+
+inline void SetButtonLedsCount(Controls &c, uint8_t count, bool on) {
+	for (auto i = 0u; i < count; i++) {
+		c.SetButtonLed(i, on);
+	}
+}
+inline void LedBlinker(Controls &c, const Shared::Blinker &b) {
+	ClearButtonLeds(c);
+	for (auto i = 0u; i < Model::NumChans; i++) {
+		c.SetButtonLed(i, b.IsHigh(i));
+	}
+}
+
+inline void SetEncoderLedsCount(Controls &c, uint8_t count, uint8_t offset, Color col) {
+	for (auto i = 0u; i < Model::NumChans; i++) {
+		auto color = (i >= offset && i < (offset + count)) ? col : Colors::off;
+		c.SetEncoderLed(i, color);
+	}
+}
+
+inline void SetEncoderLedsFloat(Controls &c, float value, Color col, Color bg_col = Colors::off) {
+	value = std::clamp(value, 0.f, 8.f);
+	auto fade_led = (unsigned)value;
+	float fade_amt = value - fade_led;
+
+	for (auto i = 0u; i < Model::NumChans; i++) {
+		auto color = (i == fade_led) ? bg_col.blend(col, fade_amt) : i < fade_led ? col : bg_col;
+		c.SetEncoderLed(i, color);
+	}
+}
+
+void DisplayRange(Controls &c, Catalyst2::Channel::Range range) {
+	const auto half = Model::NumChans / 2u;
+	const auto pos = range.PosAmount();
+	const auto neg = range.NegAmount();
+	const auto posleds = static_cast<uint8_t>(pos * half);
+	const auto negleds = static_cast<uint8_t>(neg * half);
+	const auto lastposledfade = pos * half - posleds;
+	const auto lastnegledfade = neg * half - negleds;
+	for (auto i = 0u; i < posleds; i++) {
+		c.SetEncoderLed(i + half, Palette::Voltage::Positive);
+	}
+	c.SetEncoderLed(half + posleds, Palette::off.blend(Palette::Voltage::Positive, lastposledfade));
+	for (auto i = 0u; i < negleds; i++) {
+		c.SetEncoderLed(half - 1 - i, Palette::Voltage::Negative);
+	}
+	c.SetEncoderLed(half - negleds - 1, Palette::off.blend(Palette::Voltage::Negative, lastnegledfade));
+}
+
+inline void SetLedsClockDiv(Controls &c, uint32_t div) {
+	div -= 1;
+	auto idx = 0u;
+	while (div >= 64) {
+		div -= 64;
+		idx += 1;
+	}
+
+	auto page = 0;
+	c.SetButtonLed(page, true);
+	while (div >= Model::NumChans) {
+		div -= Model::NumChans;
+		page += 1;
+		c.SetButtonLed(page, true);
+	}
+	const auto col = Palette::Setting::ClockDiv::color[idx];
+	c.SetEncoderLed(div, col);
+}
+inline void StartupAnimation(Controls &c) {
+	const auto duration = 1000;
+	auto remaining = duration;
+	ClearButtonLeds(c);
+	while (remaining--) {
+		ClearEncoderLeds(c);
+		auto phase = (duration - remaining + 0.f) / duration;
+		for (auto i = 0u; i < Model::NumChans; i++) {
+			c.SetButtonLed(i, phase);
+		}
+		phase *= 8;
+		const auto idx = static_cast<uint8_t>(phase);
+		const auto cphase = phase - idx;
+		const auto col = Palette::Scales::color[idx & 0x7];
+		const auto nextcol = idx + 1 >= 8 ? Palette::off : Palette::Scales::color[idx + 1];
+		SetEncoderLedsCount(c, 8, 0, col.blend(nextcol, cphase));
+		c.Delay(1);
+	}
+}
 
 class Abstract {
 public:
@@ -20,135 +165,6 @@ public:
 	virtual void Update(Abstract *&interface) = 0;
 	virtual void Common() = 0;
 	virtual void PaintLeds(const Model::Output::Buffer &outs) {
-	}
-
-	void LedBlinker(const Shared::Blinker &b) const {
-		ClearButtonLeds();
-		for (auto i = 0u; i < Model::NumChans; i++) {
-			c.SetButtonLed(i, b.IsHigh(i));
-		}
-	}
-
-protected:
-	void ConfirmCopy(Shared::Interface &p, uint8_t led) const {
-		p.blinker.Set(led, 8, 250, p.internalclock.TimeNow());
-	}
-	void ConfirmPaste(Shared::Interface &p, uint8_t led) const {
-		ConfirmCopy(p, led);
-	}
-	void ForEachEncoderInc(auto func) {
-		for (auto [i, enc] : countzip(c.encoders)) {
-			const auto inc = enc.read();
-			if (inc) {
-				func(i, inc);
-			}
-		}
-	}
-	void ForEachSceneButtonReleased(auto func) {
-		for (auto [i, b] : countzip(c.button.scene)) {
-			if (b.just_went_low()) {
-				func(i);
-			}
-		}
-	}
-	void ForEachSceneButtonPressed(auto func) {
-		for (auto [i, b] : countzip(c.button.scene)) {
-			if (b.just_went_high()) {
-				func(i);
-			}
-		}
-	}
-	void ClearButtonLeds() const {
-		for (auto i = 0u; i < Model::NumChans; i++) {
-			c.SetButtonLed(i, false);
-		}
-	}
-
-	void ClearEncoderLeds() {
-		for (auto i = 0u; i < Model::NumChans; i++) {
-			c.SetEncoderLed(i, Palette::off);
-		}
-	}
-
-	void SetButtonLedsCount(uint8_t count, bool on) {
-		for (auto i = 0u; i < count; i++) {
-			c.SetButtonLed(i, on);
-		}
-	}
-
-	void SetEncoderLedsCount(uint8_t count, uint8_t offset, Color col) {
-		for (auto i = 0u; i < Model::NumChans; i++) {
-			auto color = (i >= offset && i < (offset + count)) ? col : Colors::off;
-			c.SetEncoderLed(i, color);
-		}
-	}
-
-	void SetEncoderLedsFloat(float value, Color col, Color bg_col = Colors::off) {
-		value = std::clamp(value, 0.f, 8.f);
-		auto fade_led = (unsigned)value;
-		float fade_amt = value - fade_led;
-
-		for (auto i = 0u; i < Model::NumChans; i++) {
-			auto color = (i == fade_led) ? bg_col.blend(col, fade_amt) : i < fade_led ? col : bg_col;
-			c.SetEncoderLed(i, color);
-		}
-	}
-
-	void SetLedsClockDiv(uint32_t div) {
-		div -= 1;
-		auto idx = 0u;
-		while (div >= 64) {
-			div -= 64;
-			idx += 1;
-		}
-
-		auto page = 0;
-		c.SetButtonLed(page, true);
-		while (div >= Model::NumChans) {
-			div -= Model::NumChans;
-			page += 1;
-			c.SetButtonLed(page, true);
-		}
-		const auto col = Palette::Setting::ClockDiv::color[idx];
-		c.SetEncoderLed(div, col);
-	}
-
-	std::optional<uint8_t> YoungestSceneButton() {
-		auto age = 0xffffffffu;
-		uint8_t youngest = 0xffu;
-
-		for (auto [i, b] : countzip(c.button.scene)) {
-			if (!b.is_high()) {
-				continue;
-			}
-			if (b.time_high > age) {
-				continue;
-			}
-			age = b.time_high;
-			youngest = i;
-		}
-		if (youngest == 0xff) {
-			return std::nullopt;
-		}
-		return youngest;
-	}
-
-	void DisplayRange(Catalyst2::Channel::Range range) const {
-		const auto half = Model::NumChans / 2u;
-		const auto pos = range.PosAmount();
-		const auto neg = range.NegAmount();
-		const auto posleds = static_cast<uint8_t>(pos * half);
-		const auto negleds = static_cast<uint8_t>(neg * half);
-		const auto lastposledfade = pos * half - posleds;
-		const auto lastnegledfade = neg * half - negleds;
-		for (auto i = 0u; i < posleds; i++) {
-			c.SetEncoderLed(i + half, Palette::Voltage::Positive);
-		}
-		c.SetEncoderLed(half + posleds, Palette::off.blend(Palette::Voltage::Positive, lastposledfade));
-		for (auto i = 0u; i < negleds; i++) {
-			c.SetEncoderLed(half - 1 - i, Palette::Voltage::Negative);
-		}
-		c.SetEncoderLed(half - negleds - 1, Palette::off.blend(Palette::Voltage::Negative, lastnegledfade));
 	}
 };
 
