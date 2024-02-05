02df3200b60ae55e21da7feb142f594b6c8a2847
Dan Green <danngreen1@gmail.com>
2024-02-05 11:45:30 -0800

    Initial color of encoder test

--- src/hardware_tests/encoders.hh
+++ src/hardware_tests/encoders.hh
@@ -16,7 +16,7 @@ struct TestEncoders {
 
 	void run_test() {
 		for (auto i : {0, 1, 2, 3, 4, 5, 6, 7}) {
-			controls.SetEncoderLed(i, Palette::black);
+			controls.SetEncoderLed(i, Palette::yellow);
 		}
 
 		while (true) {
