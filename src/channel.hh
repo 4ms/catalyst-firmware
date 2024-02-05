548d30983462a339c32e0df2f1747a74fa41ad79
audiobird <cantrellhepner@gmail.com>
2024-02-05 12:18:19 -0800

    make gate pulse width setting more coarse

--- src/channel.hh
+++ src/channel.hh
@@ -18,9 +18,10 @@ constexpr auto octave = range / Model::output_octave_range;
 constexpr auto note = octave / 12;
 
 class Value {
-	static constexpr auto inc_step_fine = 1;
-	static constexpr auto inc_step = inc_step_fine * 25;
-	static constexpr auto min = 0, max = static_cast<int>(inc_step * 12 * Model::output_octave_range);
+	static constexpr auto inc_step_fine_cv = 1;
+	static constexpr auto inc_step_cv = inc_step_fine_cv * 25;
+	static constexpr auto min = 0, max = static_cast<int>(inc_step_cv * 12 * Model::output_octave_range);
+	static constexpr auto inc_step_gate = max / 36;
 	static constexpr auto zero =
 		MathTools::map_value(0.f, Model::min_output_voltage, Model::max_output_voltage, min, max);
 	static constexpr auto mid_point = max / 2;
@@ -51,13 +52,14 @@ public:
 		val = MathTools::map_value(v, Model::min_output_voltage, Model::max_output_voltage, min, max);
 	}
 	void IncCv(int32_t inc, bool fine, Range range) {
-		inc *= fine ? inc_step_fine : inc_step;
+		inc *= fine ? inc_step_fine_cv : inc_step_cv;
 		const auto min_ = MathTools::map_value(range.NegAmount(), .5f, 0.f, min, zero);
 		const auto max_ = MathTools::map_value(range.PosAmount(), 0.f, 1.f, zero, max);
 		val = std::clamp<int32_t>(val + inc, min_, max_);
 	}
 	void IncGate(int32_t inc) {
-		inc *= inc_step;
+		val -= val % inc_step_gate;
+		inc *= inc_step_gate;
 		val = std::clamp<int32_t>(val + inc, mid_point, max);
 	}
 	Proxy Read(Range range, float random) const {
