a4414cbdad6ce24c04b5f81cfa4e86a198d66b0f
Dan Green <danngreen1@gmail.com>
2024-02-08 14:46:19 -0800

    Re-arrange flash layout (MUST RE-FLASH BOOTLOADER)

--- src/params.hh
+++ src/params.hh
@@ -20,12 +20,23 @@ inline constexpr auto macrosize = sizeof(Macro::Data);
 inline constexpr auto macro_is_smaller = macrosize < seqsize;
 // using DataWithGlobalNonVolatileState = std::conditional<macro_is_smaller, Macro::Data, Sequencer::Data>::type;
 
-struct SequencerData : public Sequencer::Data {
+inline constexpr auto NumSeqSlots = 8u;
+
+struct SequencerData {
 	Shared::Data shared;
+	unsigned startup_slot = 0;
+	std::array<Sequencer::Data, NumSeqSlots> slots;
+
 	bool validate() const {
 		auto ret = true;
+
 		ret &= shared.Validate();
-		ret &= Sequencer::Data::validate();
+
+		for (auto &slot : slots)
+			ret &= slot.validate();
+
+		ret &= (startup_slot < NumSeqSlots);
+
 		return ret;
 	}
 };
@@ -42,7 +53,7 @@ struct Data {
 struct Params {
 	Data data;
 	Shared::Interface shared{data.sequencer.shared};
-	Sequencer::Interface sequencer{data.sequencer, shared};
+	Sequencer::Interface sequencer{data.sequencer.slots[0], shared};
 	Macro::Interface macro{data.macro, shared};
 };
 
