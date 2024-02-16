18b2310c72c1a8c257671f7098246e975e4f504c
Dan Green <danngreen1@gmail.com>
2024-02-15 16:12:16 -0800

    Failing quantizer test

--- tests/quantizer_test.cc
+++ tests/quantizer_test.cc
@@ -1,10 +1,11 @@
 #include "../src/quantizer.hh"
 
 #include "doctest.h"
+#include "util/countzip.hh"
 
 using namespace Catalyst2;
 
-TEST_CASE("Quantizer") {
+TEST_CASE("Quantizer: number of transitions is correct") {
 	// constexpr auto chromatic = QuantizerScale{0.f, 1.f, 2.f, 3.f, 4.f, 5.f, 6.f, 7.f, 8.f, 9.f, 10.f, 11.f};
 	Quantizer::Interface quan;
 
@@ -48,4 +49,33 @@ TEST_CASE("Quantizer") {
 	expected_output_values = static_cast<unsigned int>(tscale1.size() * Model::output_octave_range);
 
 	CHECK(output_values == expected_output_values);
+}
+
+TEST_CASE("Quantizer: picks closest note") {
+	constexpr auto chromatic = Quantizer::Scale{0.f, 1.f, 2.f, 3.f, 4.f, 5.f, 6.f, 7.f, 8.f, 9.f, 10.f, 11.f};
+	Quantizer::Interface quan;
+	quan.Load(chromatic);
+
+	std::array<Channel::Cv::type, (size_t)Model::output_octave_range * 12> notes;
+	for (auto [i, note] : enumerate(notes)) {
+		note = i * Channel::Cv::inc_step;
+	}
+	CHECK((Channel::Cv::octave / 12) == Channel::Cv::inc_step);
+
+	// Dead-on note value quantizes to itself
+	for (auto note : notes) {
+		CHECK(note == quan.Process(note));
+	}
+
+	// Slightly flat should quantize back to the note
+	CHECK(quan.Process(notes[1] - 1) == notes[1]);
+
+	// Slightly sharp should quantize back to the note
+	//FAILS
+	CHECK(quan.Process(notes[1] + 1) == notes[1]);
+
+	// for (auto note : notes) {
+	// 	CHECK(note == quan.Process(note - 1));
+	// 	// CHECK((note + Channel::Cv::inc_step / 2) == quan.Process(note));
+	// }
 }
