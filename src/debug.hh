bf7de08db53641477b917dda0d16792567279730
Dan Green <danngreen1@gmail.com>
2024-02-21 15:31:10 -0800

    Scoped debug pin

--- src/debug.hh
+++ src/debug.hh
@@ -8,7 +8,24 @@
 
 namespace Catalyst2::Debug
 {
+
 using Pin0 = Board::DebugPin;
+using Pins = std::tuple<Pin0>;
+
+template<unsigned PinN>
+using Pin = std::tuple_element_t<PinN, Pins>;
+
+template<unsigned PinN = 0>
+struct ScopedPin {
+	ScopedPin() {
+		Pin<PinN>::high();
+	}
+	~ScopedPin() {
+		Pin<PinN>::low();
+	}
+};
+
+static_assert(std::is_same_v<Pin<0>, Pin0>);
 }; // namespace Catalyst2::Debug
 
 #define GCC_OPTIMIZE_OFF __attribute__((optimize("-O0")))
