b69a666f38d9b583a5dca4137b34c1e558fc7ac2
Dan Green <danngreen1@gmail.com>
2024-03-29 15:14:29 -0700

    Cleanup

--- src/validate.hh
+++ src/validate.hh
@@ -1,14 +1,8 @@
 #pragma once
-
 #include <cstdint>
-#include <type_traits>
 
 namespace Catalyst2
 {
-
-static_assert(sizeof(bool) == 1);
-
-using underlying_bool_type = uint8_t;
 
 inline bool validateBool(uint8_t in) {
 	return in == 0x00 || in == 0x01;
