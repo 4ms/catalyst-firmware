1bb162743e076dfd155e26365e2b8c1a0832d489
audiobird <cantrell@madronemusic.com>
2024-05-31 09:44:51 -0700

    add blind mode setting

---
+++ src/blind.hh
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <algorithm>
+#include <cstdint>
+#include <utility>
+
+namespace Catalyst2::Macro::Blind
+{
+enum class Mode : uint8_t {
+	ON,
+	SNAP,
+	SLEW,
+};
+inline constexpr auto ModeMax = 3;
+
+struct Data {
+	Mode mode{Mode::SNAP};
+
+	bool Validate() const {
+		return std::to_underlying(mode) < ModeMax;
+	}
+};
+
+class Interface {
+	Data &data;
+
+public:
+	Interface(Data &data)
+		: data{data} {
+	}
+	void Inc(int32_t inc) {
+		auto t = std::to_underlying(data.mode);
+		t = std::clamp<int32_t>(t + inc, 0, ModeMax);
+		data.mode = static_cast<Mode>(t);
+	}
+	Mode Read() const {
+		return data.mode;
+	}
+};
+
+} // namespace Catalyst2::Macro::Blind
